9576ff067928cad55392da6117c0982ef7e8ec1e
KOLLA-SASANKA <123794309+KOLLA-SASANKA@users.noreply.github.com>
2024-07-23 20:38:31 +0530

    Update Move Zeroes.py

--- DAY-2/Move Zeroes.py
+++ DAY-2/Move Zeroes.py
@@ -1,4 +1,22 @@
-l=0
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # j=-1
+        # for i in range(len(nums)):
+        #     if nums[i]==0:
+        #         j=i
+        #         break
+        # if j==-1:
+        #     return nums
+        # for i in range(j+1,len(nums)):
+        #     if nums[i]!=0:
+        #         nums[i],nums[j]=nums[j],nums[i]
+        #         j+=1
+
+        # return nums
+        l=0
         for r in range(len(nums)):
             if nums[r]!=0:
                 nums[r],nums[l]=nums[l],nums[r]
